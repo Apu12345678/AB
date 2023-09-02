5d21e9729786ac258802c031d19556eedee0ba22
Apu12345678 <143878044+Apu12345678@users.noreply.github.com>
2023-09-02 16:33:45 +0530

    Update Arpita.py

--- Arpita.py
+++ Arpita.py
@@ -1,4 +1,10 @@
-number1 = 9
-number2 = 5
-result = number1+number2
-print(result)
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
